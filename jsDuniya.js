40352451a2582f89119de89e96fbdfd7fd6325bd
islamemon59 <islamemon59@gmail.com>
2025-11-08 21:45:31 +0600

    primitive data types

---
+++ jsDuniya.js
@@ -0,0 +1,5 @@
+let name = "Mokbul";
+
+name = 235;
+
+console.log("User Name:", name);
---
+++ src/primitive.ts
@@ -0,0 +1,19 @@
+// JS - TS
+
+// JS Types = String Number Boolean Undefined Null
+// TS Extra Types = never, unknown, void
+
+let userName: String = "Emon";
+userName = "Hossain";
+let userId = 123;
+userId = "emon";
+
+let isAdmin = true;
+isAdmin = "helllo";
+
+userName.toFixed();
+
+let x: undefined = undefined;
+
+x= "hi";
+x= 12;
